272da2a722d61ee27378187b0ad7cf2705097e90
Przemysław Sadurski <sadurski.przem@gmail.com>
2024-04-27 14:38:47 +0200

    Initial Commit

---
+++ Swift_lab2/Swift_lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_lab2App.swift
+//  Swift_lab2
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
